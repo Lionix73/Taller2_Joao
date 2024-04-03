424be2bfdca969440e2c2f21a85db705ab7136b7
Alejandro Lopez <alejolr1218@gmail.com>
2024-04-03 11:38:49 -0500

    Void Texto Usuario

---
+++ Ejercicio_Delegado/Ejercicio_Delegado/Program.cs
@@ -0,0 +1,37 @@
+﻿
+//Delegados
+/*delegate bool VerificarInput(string inputUsuario);
+
+static bool VerificarInput(string inputUsuario)
+{
+
+}*/
+
+//Variables Globales
+
+//Llamado Funciones
+TextoUsuario();
+
+void TextoUsuario()
+{
+    List<int> respuestaUsuario = new List<int>();
+    int numeroRespuesta;
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"Ingresa una 5 numeros enteros diferentes para un promedio. \n\nNumero #{i + 1}: \n");
+        string tempInput = Console.ReadLine();
+
+        if (int.TryParse(tempInput, out numeroRespuesta))
+        {
+            respuestaUsuario.Add(numeroRespuesta);
+            Console.Clear();
+        }
+        else
+        {
+            i--;
+            Console.Clear();
+            Console.WriteLine("Texto Ingresado invalido\n");
+        }
+    }
+}
